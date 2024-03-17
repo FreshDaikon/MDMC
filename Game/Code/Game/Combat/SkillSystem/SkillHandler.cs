62c3a0463a31762ac41c8d0e76b9b839c88f83df
FreshDaikon <36603973+FreshDaikon@users.noreply.github.com>
2024-03-17 18:53:53 +0100

    a small step for gnomes.

--- Game/Code/Game/Combat/SkillSystem/SkillHandler.cs
+++ Game/Code/Game/Combat/SkillSystem/SkillHandler.cs
@@ -7,60 +7,60 @@ namespace Mdmc.Code.Game.Combat.SkillSystem;
 [GlobalClass]
 public partial class SkillHandler: Node
 {
-    public enum SkillType
-    {
-        GCD,
-        OGCD
-    }
+	public enum SkillType
+	{
+		GCD,
+		OGCD
+	}
 
-    [Export] private SkillType _type;
-    [Export] private TimeController _timeController;
-    [Export] private Mdmc.Code.Game.Combat.SkillSystem.ActionController _actionController;
+	[Export] private SkillType _type;
+	[Export] private TimeController _timeController;
+	[Export] private Mdmc.Code.Game.Combat.SkillSystem.ActionController _actionController;
 
-    public PlayerArsenal Arsenal { get; private set; }
-    public SkillData Data { get; private set; }
-    public int AssignedSlot { get; private set; }
+	public PlayerArsenal Arsenal { get; private set; }
+	public SkillData Data { get; private set; }
+	public int AssignedSlot { get; private set; }
 
-    // Initializers:
-    public void SetArsenal(PlayerArsenal arsenal) => Arsenal = arsenal;
-    
-    public void SetData(SkillData data)
-    { 
-        Data = data;
-    }
-    public void AssignSlot(int slot) => AssignedSlot = slot;
-    // Getters:
-    public TimeController GetTimeInfo() => _timeController;
-    public SkillType GetTypeInfo() => _type;
+	// Initializers:
+	public void SetArsenal(PlayerArsenal arsenal) => Arsenal = arsenal;
+	
+	public void SetData(SkillData data)
+	{ 
+		Data = data;
+	}
+	public void AssignSlot(int slot) => AssignedSlot = slot;
+	// Getters:
+	public TimeController GetTimeInfo() => _timeController;
+	public SkillType GetTypeInfo() => _type;
 
 
-    public override void _Ready()
-    {
-        if(_timeController == null) return;
-        if(_actionController == null) return;
-        if(!Multiplayer.IsServer()) return;
-        _actionController.ActionsTriggered += OnActionsTriggered;
-    }
+	public override void _Ready()
+	{
+		if(_timeController == null) return;
+		if(_actionController == null) return;
+		if(!Multiplayer.IsServer()) return;
+		_actionController.ActionsTriggered += OnActionsTriggered;
+	}
 
-    public void OnActionsTriggered()
-    {
-        if(_timeController.CurrentCooldown > 0) { _timeController.StartCooldown(); }
-    }
+	public void OnActionsTriggered()
+	{
+		if(_timeController.CurrentCooldown > 0) { _timeController.StartCooldown(); }
+	}
 
-    public SkillResult CheckSkill()
-    {
-        if(_timeController.IsOnCooldown)
-        {
-            return new SkillResult(){ SUCCESS = false, result = Mdmc.Code.System.MD.ActionResult.ON_COOLDOWN };
-        }
-        return _actionController.CanActivate();
-    }
+	public SkillResult CheckSkill()
+	{
+		if(_timeController.IsOnCooldown)
+		{
+			return new SkillResult(){ SUCCESS = false, result = Mdmc.Code.System.MD.ActionResult.ON_COOLDOWN };
+		}
+		return _actionController.CanActivate();
+	}
 
-    public SkillResult TriggerSkill()
-    {
-        if(!Multiplayer.IsServer()) return new SkillResult(){ SUCCESS = false, result = Mdmc.Code.System.MD.ActionResult.NOT_SERVER};
-        var check = CheckSkill();
-        if(!check.SUCCESS) return check;
-        return _actionController.ActivateAction();
-    }  
+	public SkillResult TriggerSkill()
+	{
+		if(!Multiplayer.IsServer()) return new SkillResult(){ SUCCESS = false, result = Mdmc.Code.System.MD.ActionResult.NOT_SERVER};
+		var check = CheckSkill();
+		if(!check.SUCCESS) return check;
+		return _actionController.ActivateAction();
+	}  
 }
--- Game/Code/System/Loader.cs
+++ Game/Code/System/Loader.cs
@@ -5,38 +5,38 @@ namespace Mdmc.Code.System;
 
 public partial class Loader : Node
 {
-    //Dependency Scenes:
-    [Export(PropertyHint.File)]
-    private string ServerPath;
-    [Export(PropertyHint.File)]
-    private string ClientPath;
+	//Dependency Scenes:
+	[Export(PropertyHint.File)]
+	private string ServerPath;
+	[Export(PropertyHint.File)]
+	private string ClientPath;
 
    
-    public override void _Ready()
-    {
-        GD.Print("Starting MDMC");
-        var args = MD.GetArgs();
-        GD.Print(args);
-        //First check for Server:
-        if(args.ContainsKey("gameserver"))
-        {
-            var ServerSceneRes = (PackedScene)ResourceLoader.Load(ServerPath);
-            var server = ServerSceneRes.Instantiate();
-            AddChild(server);
-        }
-        else if(args.ContainsKey("playfab"))
-        {
-            var ServerSceneRes = (PackedScene)ResourceLoader.Load(ServerPath);
-            var server = ServerSceneRes.Instantiate();
-            AddChild(server);
-        }
-        // We are a Client:
-        else
-        {
-            GD.Print("Starting as client..");
-            var ClientSceneRes = (PackedScene)ResourceLoader.Load(ClientPath);
-            var client = ClientSceneRes.Instantiate();
-            AddChild(client);
-        }
-    }
+	public override void _Ready()
+	{
+		GD.Print("Starting MDMC");
+		var args = MD.GetArgs();
+		GD.Print(args);
+		//First check for Server:
+		if(args.ContainsKey("gameserver"))
+		{
+			var ServerSceneRes = (PackedScene)ResourceLoader.Load(ServerPath);
+			var server = ServerSceneRes.Instantiate();
+			AddChild(server);
+		}
+		else if(args.ContainsKey("playfab"))
+		{
+			var ServerSceneRes = (PackedScene)ResourceLoader.Load(ServerPath);
+			var server = ServerSceneRes.Instantiate();
+			AddChild(server);
+		}
+		// We are a Client:
+		else
+		{
+			GD.Print("Starting as client..");
+			var ClientSceneRes = (PackedScene)ResourceLoader.Load(ClientPath);
+			var client = ClientSceneRes.Instantiate();
+			AddChild(client);
+		}
+	}
 }
